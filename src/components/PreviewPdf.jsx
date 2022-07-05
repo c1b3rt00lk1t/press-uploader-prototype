befd4ddbdf7e2d0a4e4fc3d419057dbf7b64b602
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-05 21:29:34 +0200

    Error Boundary included to wrap the PreviewPdf.

--- src/components/PreviewPdf.jsx
+++ src/components/PreviewPdf.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { ErrorBoundary } from "./ErrorBoundary";
 import PDFDocument from "./PDFDocument";
 
 const PreviewPdf = ({ selectedFileTitle, selectedFileSource, files }) => {
-  const url = files.filter((file) => file.name.includes(selectedFileTitle))[0]
-  const label = selectedFileSource !== 'label' ? true : false;
+  const url = files.filter((file) => file.name.includes(selectedFileTitle))[0];
+  const label = selectedFileSource !== "label" ? true : false;
   return (
     <div className="previewPdf">
-       {label && <PDFDocument url={url}/>}
-        
-
-
+      {label && (
+        <ErrorBoundary>
+          <PDFDocument url={url} />
+        </ErrorBoundary>
+      )}
     </div>
   );
 };
