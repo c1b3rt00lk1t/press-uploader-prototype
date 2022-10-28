fccfcd8dc2dcab1fc67e6bbef9161153cffd54a8
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-28 17:20:05 +0200

    Added component File into Folder.

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,17 +1,8 @@
 import React from "react";
+import File from "./File";
 
 const Folder = ({ folder, draggableFiles, srcFilesObj }) => {
-  const tagStyle = {
-    fontSize: "10px",
-    border: "solid",
-    borderRadius: "5px",
-    borderWidth: "1px",
-    marginRight: "5px",
-    paddingLeft: "2px",
-    textAlign: "center",
-    lineHeight: "20px",
-    height: "20px",
-  };
+  
 
   return (
     <li
@@ -26,36 +17,7 @@ const Folder = ({ folder, draggableFiles, srcFilesObj }) => {
       {folder.folder}
       <ul>
         {folder.files.map((file) => (
-          <li
-            className="orderFile"
-            draggable={draggableFiles}
-            onDragStart={(ev) => {
-              ev.stopPropagation();
-              ev.dataTransfer.setData("id", file.id);
-            }}
-            key={file.id}
-            id={file.id}
-            style={{
-              color:
-                srcFilesObj && srcFilesObj[file.id] !== folder.id
-                  ? "rgb(220, 0, 0)"
-                  : "black",
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-              whiteSpace: "nowrap",
-            }}
-          >
-            {srcFilesObj &&
-              srcFilesObj[file.id] !== folder.id &&
-              (srcFilesObj[file.id] ? (
-                <span style={tagStyle}>
-                  check: {srcFilesObj && srcFilesObj[file.id]}{" "}
-                </span>
-              ) : (
-                <span style={tagStyle}>not found </span>
-              ))}{" "}
-            {file.id}
-          </li>
+          <File draggableFiles={draggableFiles} folder={folder} file={file} srcFilesObj={srcFilesObj}/>
         ))}
       </ul>
     </li>
