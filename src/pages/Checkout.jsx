6110c2852a074b2400635f9294fac4bec446e0a7
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-13 23:00:53 +0200

    Dummy checkout tests 2.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from "../shared/Card";
 import CardDisplay from "../shared/CardDisplay";
 
 const Checkout = () => {
+
+  const [checkoutSession, setCheckoutSession] = useState("");
+  const [checkoutSessionURL, setCheckoutSessionURL] = useState("");
+
   return (
     <CardDisplay>
-        <Card status={true} msg={['Hello']}>
-          <button onClick={() => {console.log('bye')}}>Checkout</button>
+        <Card status={undefined} msg={['']}>
+            <div style={{display:'flex', flexDirection:'column', justifyContent: "space-around", height:"5vh"}}>
+                <input type="text" placeholder="session" value={checkoutSession} onChange={e => setCheckoutSession(e.target.value)}></input>
+                <input type="text" placeholder="download url" value={checkoutSessionURL} onChange={e => setCheckoutSessionURL(e.target.value)}></input>
+            </div>
+          <button onClick={() => {
+            console.log('fake checkout',checkoutSession,checkoutSessionURL)
+            setCheckoutSession("");
+            setCheckoutSessionURL("");
+        }}>Checkout</button>
         </Card>
     </CardDisplay>
   )
