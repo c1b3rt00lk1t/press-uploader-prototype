e7e8bb729209fd5495a2b260fc844b98bb830c2b
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-15 20:52:28 +0200

    Check that the SearchBox component renders a placeholder

--- src/components/SearchBox.test.js
+++ src/components/SearchBox.test.js
@@ -4,13 +4,10 @@ import { render, screen } from "@testing-library/react";
 
 import SearchBox from "./SearchBox";
 
-describe("Testing the Search box", () =>{
-
-    test("Should render content", () => {
-        render(<SearchBox />)
-    });
-
-    const element = screen.getByText('SearchBox')
-    expect(element).toBeDefined()
-
+describe("Testing the Search box", () => {
+  test("Should render content", () => {
+    render(<SearchBox />);
+    const element = screen.getByPlaceholderText(/Type a zone, sector or tag.../i);
+    expect(element).toBeDefined();
+  });
 });
