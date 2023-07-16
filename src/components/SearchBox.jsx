558668ec9878c022041dfad160aa3311e5019d7d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-17 01:16:52 +0200

    Hides the component as it is not ready to be used in prod.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBox = ({handleSearchBoxFocus, handleSearchBoxBlur}) => {
   return (
-    <input onChange={(ev) => {console.log(ev.target.value)}} onFocus={handleSearchBoxFocus} onBlur={handleSearchBoxBlur} placeholder="Type a zone, sector or tag..."></input>
+    <input style={{position: 'absolute', visibility: 'hidden'}} onChange={(ev) => {console.log(ev.target.value)}} onFocus={handleSearchBoxFocus} onBlur={handleSearchBoxBlur} placeholder="Type a zone, sector or tag..."></input>
   )
 }
 
