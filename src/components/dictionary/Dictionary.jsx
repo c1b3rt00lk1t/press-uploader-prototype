67e4e44316a4b1940f0eb8da6c15b61c41a79054
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-16 23:30:21 +0200

    Remove a couple more of unnecessary comments.

--- src/components/dictionary/Dictionary.jsx
+++ src/components/dictionary/Dictionary.jsx
@@ -19,8 +19,6 @@ const Dictionary = ({
   handleGetPreviousTags,
 }) => {
   const {
-    // handleUploadDictionary,
-    // handleGetDictionaryFromDB,
     dictionary,
   } = useContext(PressUploaderContext);
 
