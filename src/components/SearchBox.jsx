d82054d7d7784f4636ff9b92ac15fdac74a6a710
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-18 20:51:03 +0200

    Destructuring logic extracted to in a useEffect hook

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const SearchBox = ({
   handleSearchBoxFocus,
@@ -17,6 +17,35 @@ const SearchBox = ({
   const [flatSectors, setFlatSectors] = useState([]);
   const [flatTags, setFlatTags] = useState([]);
 
+  useEffect(() => {
+      // temp variables for destructring any of the three trees
+      const zones = [];
+      const sectors = [];
+      const tags = [];
+
+      // generic recursive function for destructring any of the three trees
+      const destructureItems = (target) => (path) => (object) => {
+        for (let [prop, value] of Object.entries(object)) {
+          if (typeof value === "object") {
+            // `${path}/${prop}` is kept in order to unfold also the target subfolder
+            // to avoid this behavior and be more restrictive with unfolding, the /${prop} could be removed
+            target.push({ item: prop, path: `${path}/${prop}` });
+            destructureItems(target)(`${path}/${prop}`)(value);
+          } else if (typeof value === "boolean") {
+            target.push({ item: prop, path: `${path}` });
+          }
+        }
+        return target;
+      };
+
+      // the result of the calculation is stored in the corresponding states
+      setFlatZones(destructureItems(zones)("zones")(dictionary.zones));
+      setFlatSectors(destructureItems(sectors)("sectors")(dictionary.sectors));
+      setFlatTags(destructureItems(tags)("tags")(dictionary.tags));
+  },[dictionary.sectors, dictionary.tags, dictionary.zones])
+  
+
+
   const handleChangeSearchBox = (ev) => {
     setInputValue(ev.target.value);
     // if the length of the input is below the 2 characters, everything is kept folded
@@ -25,28 +54,6 @@ const SearchBox = ({
       setSearched([]);
       return;
     }
-
-    const zones = [];
-    const sectors = [];
-    const tags = [];
-
-    // generic recursive function for destructring any of the three trees
-    const destructureItems = (target) => (path) => (object) => {
-      for (let [prop, value] of Object.entries(object)) {
-        if (typeof value === "object") {
-          // `${path}/${prop}` is kept in order to unfold also the target subfolder
-          // to avoid this behavior and be more restrictive with unfolding, the /${prop} could be removed
-          target.push({ item: prop, path: `${path}/${prop}` });
-          destructureItems(target)(`${path}/${prop}`)(value);
-        } else if (typeof value === "boolean") {
-          target.push({ item: prop, path: `${path}` });
-        }
-      }
-      return target;
-    };
-    setFlatZones(destructureItems(zones)("zones")(dictionary.zones));
-    setFlatSectors(destructureItems(sectors)("sectors")(dictionary.sectors));
-    setFlatTags(destructureItems(tags)("tags")(dictionary.tags));
 
     // it gets the event target value and filters with it the list of items to obtain their path
     // the result is an array of strings that has to be joined in a string first
@@ -66,13 +73,13 @@ const SearchBox = ({
     setAllUnfoldedEmpty();
 
     // Once the zones to be unfolded are identified, they are unfolded by the basic setter function called in a functional way and preventing duplicates to be stored (they wouldn't break anything but are not clean)
-    toBeUnfolded(zones).forEach((unfold) =>
+    toBeUnfolded(flatZones).forEach((unfold) =>
       setUnfoldedZones((prev) => [...new Set(prev.concat(unfold))])
     );
-    toBeUnfolded(sectors).forEach((unfold) =>
+    toBeUnfolded(flatSectors).forEach((unfold) =>
       setUnfoldedSectors((prev) => [...new Set(prev.concat(unfold))])
     );
-    toBeUnfolded(tags).forEach((unfold) =>
+    toBeUnfolded(flatTags).forEach((unfold) =>
       setUnfoldedTags((prev) => [...new Set(prev.concat(unfold))])
     );
 
@@ -89,9 +96,9 @@ const SearchBox = ({
 
     setSearched([
       ...new Set(
-        searching(zones)
-          .concat(searching(sectors))
-          .concat(searching(tags))
+        searching(flatZones)
+          .concat(searching(flatSectors))
+          .concat(searching(flatTags))
           .filter((item) => item !== "")
       ),
     ]);
