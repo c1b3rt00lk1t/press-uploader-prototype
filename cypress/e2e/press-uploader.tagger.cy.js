5e9f920662ec9ae2413acdb368ed7c2071bf2f1e
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-28 09:14:29 +0200

    test cases for checking tags.

--- cypress/e2e/press-uploader.tagger.cy.js
+++ cypress/e2e/press-uploader.tagger.cy.js
@@ -56,7 +56,52 @@ describe('Uploader :: Tagger', () => {
       cy.get('.previous').should('have.length',5)
     })
 
+    xit('checks a selected tag when it is the only possible one and enter key is pressed', () => {
+      // with the first three letters 'ale', there are two tags available
+      // the two tags should appear in a variation of red
+      // the enter keydown has no effect
+      cy.get('input[placeholder*="Type a zone, sector or tag..."]').type('ale')
+      cy.get('.check-box').contains('alemania').should('have.css', 'color', 'rgb(220, 20, 60)')
+      cy.get('.check-box').contains('palestina').should('have.css', 'color', 'rgb(220, 20, 60)')
+      cy.get('body').type('{enter}')
+      cy.get('.check-box').contains('alemania').should('have.css', 'color', 'rgb(220, 20, 60)')
+
+      // with the fourth letter 'm', only one option is available
+      // it should display in green
+      cy.get('input[placeholder*="Type a zone, sector or tag..."]').type('m')
+      cy.get('.check-box').contains('alemania').should('have.css', 'color', 'rgb(0, 128, 0)')
+      cy.get('.check-box').contains('alemania').parent().should('have.css', 'background-color', 'rgba(60, 114, 60, 0.1)')
+      // now the enter keydown has the effect of checking the tag
+      // the tag appears blue when checked as does the checkbox
+      cy.get('body').type('{enter}')
+      cy.get('.check-box').contains('alemania').should('have.css', 'color', 'rgb(0, 0, 255)')
+      cy.get(':has(+ .dictionary-checked)').should('have.css', 'color', 'rgb(0, 0, 255)')
+
+    })
   
+    it('checks a selected tag when it is not the only possible one but the input matches the tag and enter key is pressed', () => {
+      // with the letters 'preci', there are some tags available
+      // the two tags should appear in a variation of red
+      // the enter keydown has no effect
+      cy.get('input[placeholder*="Type a zone, sector or tag..."]').type('preci')
+      cy.get('.check-box').contains(/^precio$/).should('have.css', 'color', 'rgb(220, 20, 60)')
+      cy.get('.check-box').contains('precios industriales').should('have.css', 'color', 'rgb(220, 20, 60)')
+      cy.get('body').type('{enter}')
+      cy.get('.check-box').contains('precio').should('have.css', 'color', 'rgb(220, 20, 60)')
+
+      // with the letter 'o', there are many options available but one that matches the input
+      // it should display with the same color but with the background highlighted in orange
+      cy.get('input[placeholder*="Type a zone, sector or tag..."]').type('o')
+      cy.get('.check-box').contains('precio').should('have.css', 'color', 'rgb(220, 20, 60)')
+      cy.get('.check-box').contains(/^precio$/).parent().should('have.css', 'background-color', 'rgba(255, 183, 0, 0.1)')
+
+      // now the enter keydown has the effect of checking the tag
+      // the tag appears blue when checked as does the checkbox
+      cy.get('body').type('{enter}')
+      cy.get('.check-box').contains(/^precio$/).should('have.css', 'color', 'rgb(0, 0, 255)')
+      // cy.get(':has(+ .dictionary-checked)').should('have.css', 'color', 'rgb(0, 0, 255)')
+
+    })
   
   })
   
