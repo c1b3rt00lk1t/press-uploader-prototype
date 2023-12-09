14be0c3135c579a9bf27113e766c6f05fb68ef0a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-09 10:57:27 +0100

    Primary Firebase project configuration

--- src/firebase.js
+++ src/firebase.js
@@ -6,58 +6,58 @@ import {
   uploadBytes,
   getDownloadURL,
 } from "firebase/storage";
-import { getDatabase, ref as refDb, set, update, onValue } from "firebase/database";
+import {
+  getDatabase,
+  ref as refDb,
+  set,
+  update,
+  onValue,
+} from "firebase/database";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Primary Firebase project configuration (for demo purposes)
 const firebaseConfig = {
-  apiKey: "AIzaSyDlDzlrEY-VkPZdNkEZOTSbrLJcL2LoQTo",
-  authDomain: "press-uploader.firebaseapp.com",
-  projectId: "press-uploader",
-  storageBucket: "press-uploader.appspot.com",
-  messagingSenderId: "496555378224",
+  apiKey: "AIzaSyA_-oNiXKfLQ3xojZ2uIVKzB30Y28M95jY",
+  authDomain: "press-uploader-2348f.firebaseapp.com",
+  projectId: "press-uploader-2348f",
+  storageBucket: "press-uploader-2348f.appspot.com",
+  messagingSenderId: "687171451792",
   databaseURL:
-    "https://press-uploader-default-rtdb.europe-west1.firebasedatabase.app/",
-  appId: "1:496555378224:web:323bd87185f34dfe5c8ff5",
+    "https://press-uploader-2348f-default-rtdb.europe-west1.firebasedatabase.app/",
+  appId: "1:687171451792:web:b997e317fd16fc05389115",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig, 'app');
+const app = initializeApp(firebaseConfig, "app");
 
 /** Authorization */
 const auth = getAuth(app);
 
 export const authenticateUser = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password)
-  .then(() => {
-    return true;
-  })
-  .catch(() => {
-    return false
-  });
-}
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+};
 
 export const signOutUser = () => {
-  signOut(auth).then(() => {
-    console.log("Logged out")
-  }).catch(() => {
-    // An error happened.
-  });
-}
-
+  signOut(auth)
+    .then(() => {
+      console.log("Logged out");
+    })
+    .catch(() => {
+      // An error happened.
+    });
+};
 
 /** Cloud Storage */
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
 // Initialize Real-time database
 const database = getDatabase(app);
-
 
 export const uploadFile = (file, path) => {
   const storageRef = refStorage(storage, path);
@@ -75,10 +75,9 @@ export const getFileURL = async (path) => {
 
 // The update is restricted to a session in order to avoid bigger damages
 
-
-const writeData =  (path) => async (data,subpath) => {
-  if(!subpath){
-      subpath = ''
+const writeData = (path) => async (data, subpath) => {
+  if (!subpath) {
+    subpath = "";
   }
   // console.log(path + subpath)
   // console.log(data)
@@ -93,27 +92,25 @@ export const writeDataSession = writeData("/sessions/");
 export const writeDataDictionary = writeData("/dictionary/");
 export const writeDataSysSession = writeData("/sys/sessions/");
 
-const updateData = (path) => async (data, subpath) =>{
+const updateData = (path) => async (data, subpath) => {
   try {
     await update(refDb(database, path + subpath), data);
   } catch (error) {
     return false;
   }
   return true;
-}
+};
 
 export const updateDataDictionary = updateData("/dictionary");
 
 const getDataFromDB = (path) => (handleDataFromDB) => {
- 
   const refDB = refDb(database, path);
-   onValue(refDB,  (snapshot) => {
-    const data =  snapshot.val();   
+  onValue(refDB, (snapshot) => {
+    const data = snapshot.val();
     handleDataFromDB(data);
   });
 };
 
-export const getDataFromDBSession =  getDataFromDB("/sessions/");
-export const getDataFromDBDictionary =  getDataFromDB("/dictionary/");
-export const getDataFromDBSessionList =  getDataFromDB("/sys/sessions/");
-
+export const getDataFromDBSession = getDataFromDB("/sessions/");
+export const getDataFromDBDictionary = getDataFromDB("/dictionary/");
+export const getDataFromDBSessionList = getDataFromDB("/sys/sessions/");
