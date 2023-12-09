1b030212891fae50ea9cc941fc00eaca1edc7d16
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-09 10:57:38 +0100

    Secondary Firebase project configuration

--- src/firebase2.js
+++ src/firebase2.js
@@ -1,5 +1,5 @@
-/** 
- * LOGIC FOR THE BACKUP SERVER
+/**
+ * LOGIC FOR THE BACKUP PROJECT
  */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -8,22 +8,21 @@ import {
   ref as refStorage,
   uploadBytes,
   getDownloadURL,
-  getMetadata
+  getMetadata,
 } from "firebase/storage";
 
-
-// Your web app's Firebase configuration
+// Secondary Firebase project configuration (for demo purposes)
 const firebaseConfig = {
-  apiKey: "AIzaSyDUy1hFE-VOkWqOS0FmeNakeqp531kVdx0",
-  authDomain: "press-uploader-2.firebaseapp.com",
-  projectId: "press-uploader-2",
-  storageBucket: "press-uploader-2.appspot.com",
-  messagingSenderId: "75363353539",
-  appId: "1:75363353539:web:11077c15544e85eed47376"
+  apiKey: "AIzaSyCHqz9wa0g7dNabhkPn_Pc6pG588Bl_O7c",
+  authDomain: "press-uploader-afe86.firebaseapp.com",
+  projectId: "press-uploader-afe86",
+  storageBucket: "press-uploader-afe86.appspot.com",
+  messagingSenderId: "1087909215520",
+  appId: "1:1087909215520:web:73d7567ca71d508ff4ab45",
 };
 
 // Initialize Firebase
-const app2 = initializeApp(firebaseConfig, 'app2');
+const app2 = initializeApp(firebaseConfig, "app2");
 
 /** Cloud Storage */
 // Initialize Cloud Storage and get a reference to the service
@@ -38,6 +37,8 @@ export const getFileURLFromBackUp = async (path) => {
   const storageRef = refStorage(storage, path);
   const downloadURL = await getDownloadURL(storageRef);
   // From the backup, also the size is gotten
-  const fileSizeBytes = await getMetadata(storageRef).then((metadata) => metadata.size);
-  return {url: downloadURL, size: fileSizeBytes};
+  const fileSizeBytes = await getMetadata(storageRef).then(
+    (metadata) => metadata.size
+  );
+  return { url: downloadURL, size: fileSizeBytes };
 };
